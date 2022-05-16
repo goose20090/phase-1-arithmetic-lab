f987991f330eda9823d2c6ed5b24a42310328b48
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-05-16 19:25:44 +0100

    commit tree 935a49426b9ab0ebcacc6e01c14b1360e9c12f34

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1= 31;
+let num2= 2;
+let multiply = num1*num2;
+
+let random= Math.floor(Math.random() * 10)+ 1;
+random;
+
+let num3 = 16;
+let num4 = 4;
+
+const mod= num3/num4;
+
+let max= Math.max(1,2,3,4,20);
+ 
 
 // Write your code here
